cb8259634a3c84a97a52bfb6d9e67b1956a473f5
Simone Todaro <simone@cyber-duck.co.uk>
2016-04-16 10:39:53 +0200

    Fix constructor signature in exporter contract

--- src/Contract/ExporterInterface.php
+++ src/Contract/ExporterInterface.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface ExporterInterface
 {
-    public function load(Collection $data, $sheet = 'Sheet1');
+    public function load(Collection $data);
     public function setSerialiser(SerialiserInterface $serialiser);
     public function save($filename);
     public function stream($filename);
